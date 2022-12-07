6dadf94c278e665a1179e05731fc93e23d2f4a8c
divide <ldvdzhang@gmail.com>
2022-12-07 06:08:40 -0500

    123

--- cnn.py
+++ cnn.py
@@ -3,16 +3,78 @@ from  import_dataset import Dataset
 import tensorflow as tf
 import os
 import random
+import torch
+import torchvision
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image
 from tqdm import tqdm
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+from sklearn.model_selection import train_test_split
 
-image_size = 256
+# Hyper params
+image_size = 224
 batch = 8
-epoch = 5
+num_epoch = 3
+learning_rate = 0.005
+num_classes = 2
 
+# Device config
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=2):
+        super(AlexNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            nn.BatchNorm2d(96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU())
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(6400, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(4096, 2))
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+model = AlexNet(num_classes).to(device)
 
 if(os.path.exists("image.pickle") and os.path.exists("label.pickle")):
     with open('image.pickle', 'rb') as handle:
@@ -21,17 +83,100 @@ if(os.path.exists("image.pickle") and os.path.exists("label.pickle")):
         label_process = pickle.load(handle)
 else:
     dataset = Dataset()
-    image_process, label_process = preProcessing.processing(dataset,256) #image's size is 256
-    with open('dataset.pickle', 'wb') as handle:
+    image_process, label_process = preProcessing.processing(dataset,224) #image's size is 224
+    with open('image.pickle', 'wb') as handle:
         pickle.dump(image_process, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('label.pickle', 'wb') as handle:
         pickle.dump(label_process, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-dataset = tf.data.Dataset.from_tensor_slices((image_process, label_process))
-train_dataset_num = int(len(image_process) * 0.9)
-test_dataset_num = len(image_process) - train_dataset_num
-train_dataset = dataset.shuffle(train_dataset_num).repeat()
+# dataset = tf.data.Dataset.from_tensor_slices((image_process, label_process))
 
-test_dataset = dataset.shuffle(test_dataset_num).repeat()
-print("train_dataset: " + str(train_dataset.as_numpy_iterator()))
+transform = transforms.Compose([
+    transforms.ToTensor()
+    ,transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.2, 0.2, 0.2])
+])
+
+
+X_train, X_test, y_train, y_test = train_test_split(image_process, label_process,
+                                                    test_size=0.2, shuffle = True, random_state = 8)
+
+# Use the same function above for the validation set
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                  test_size=0.25, random_state= 8)
+
+print("X_train shape: {}".format(len(X_train)))
+print("X_test shape: {}".format(len(X_test)))
+print("y_train shape: {}".format(len(y_train)))
+print("y_test shape: {}".format(len(y_test)))
+print("X_val shape: {}".format(len(y_train)))
+print("y val shape: {}".format(len(y_test)))
+
+
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.005, momentum = 0.9)
+
+
+# Train the model
+total_step = len(X_train)
+
+for epoch in range(num_epoch):
+    print("total lenth is ", len(X_train))
+    for i in range(len(X_train)):
+        print("running with", i)
+        # Convert images to tensor
+        images = X_train[i]
+        if images == 123: # the cropped image is invalid
+            continue
+        images = transform(images)
+        # now image is tensor of image
+
+        labels = torch.tensor(int(y_train[i]))
+
+        # Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        images_t = torch.unsqueeze(images, 0)
+        outputs = model(images_t)
+
+        labels_t = torch.unsqueeze(labels, 0)
+        loss = criterion(outputs, labels_t)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+           .format(epoch+1, num_epoch, i+1, total_step, loss.item()))
+
+    # Validation
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for i in range(len(X_val)):
+            # Convert images to tensor
+            images = X_val[i]
+            images = transform(images)
+            # now image is tensor of image
+
+            labels = torch.tensor(int(y_val[i]))
+
+            images = images.to(device)
+            labels = labels.to(device)
+            images_t = torch.unsqueeze(images, 0)
+            outputs = model(images_t)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels_t.size(0)
+            correct += (predicted == labels_t).sum().item()
+            del images, labels, outputs
+
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
+
+
+
 
 
--- preProcessing_data.py
+++ preProcessing_data.py
@@ -2,40 +2,71 @@ import tensorflow as tf
 from tensorflow import keras
 #from tensorflow.keras import layers
 import matplotlib.pyplot as plt
+from PIL import Image
 from import_dataset import Dataset
 import numpy as np
 from tqdm import tqdm
 import warnings
+from torchvision import transforms
 
 class preProcessing:
     @staticmethod
     def processing(dataset, pic_size):
+
+        transform = transforms.Compose([
+            transforms.Resize((224,224))
+            #,transforms.Normalize(mean=[0.468, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
         #Resize each image with pic_size
         image_process = []
         label_process = []
-        for i in tqdm(range(1801,1805)):
+        for i in tqdm(range(1801,1900)):
             try:
                 image, label, cropped_images = dataset[i]
             except:
                 continue
             #print(i)
             if(len(cropped_images) != 0):
-                if(label['NumOfAnno'] == 1):      # Check cropped image is 2d or 3d
-                    image_resize = tf.image.resize(cropped_images, [pic_size, pic_size])
-                    image_norm = tf.cast(image_resize, tf.float32) / 255
-                    if(len(image_norm) != 256):
-                        image_norm = image_norm[0]
-                    image_process.append(image_norm)
-                    label_process.append(label['Annotations'][0]['isProtected'])
+                if(label['NumOfAnno'] == 1):     # Check cropped image is 2d or 3d
+                    try:
+                        PIL_image = Image.fromarray(cropped_images[0])
+                        image_resize = transform(PIL_image)
+                    except:
+                        # image_process.append(123) # the cropped is not valid
+                        # label_process.append(label['Annotations'][0]['isProtected'])
+                        continue
+                    # image_resize = tf.image.resize(cropped_images, [pic_size, pic_size])
+                    # image_norm = tf.cast(image_resize, tf.float32) / 255
+                    # if(len(image_norm) != 256):
+                    #     image_norm = image_norm[0]
+                    image_process.append(image_resize)
+
+                    # if()
+
+                    # label_process.append(label['Annotations'][0]['isProtected'])
+                    if(label['Annotations'][0]['classname'] == 'face_with_mask'):
+                        label_process.append(True)
+                    else:
+                        label_process.append(False)
                 else:
                     for j in range(len(cropped_images)):
                         if(len(cropped_images[j]) != 0):       #Some images' crop only have 1 pixel, ignore them all
-                            image_resize = tf.image.resize(cropped_images[j], [pic_size, pic_size])
-                            image_norm = tf.cast(image_resize, tf.float32) / 255
-                            if(len(image_norm) != 256):
-                                image_norm = image_norm[0]
-                            image_process.append(image_norm)
-                            label_process.append(label['Annotations'][j]['isProtected'])
+
+                            PIL_image = Image.fromarray(cropped_images[j])
+                            image_resize = transform(PIL_image)
+
+
+                            # image_resize = tf.image.resize(cropped_images[j], [pic_size, pic_size])
+                            # image_norm = tf.cast(image_resize, tf.float32) / 255
+                            # if(len(image_norm) != 256):
+                            #     image_norm = image_norm[0]
+                            image_process.append(image_resize)
+                            # label_process.append(label['Annotations'][j]['isProtected'])
+                            if(label['Annotations'][j]['classname'] == 'face_with_mask'):
+                                label_process.append(True)
+                            else:
+                                label_process.append(False)
         return image_process, label_process
 
 # #warnings.filterwarnings(("ignore"))
